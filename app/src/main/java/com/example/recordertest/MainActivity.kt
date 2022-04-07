738783f920c0660f59aded7358df3c35ea22c686
Last-SilverLight-End <halfwing1998@gmail.com>
2022-04-07 21:23:37 +0900

    minimun recording fixed

--- app/src/main/java/com/example/recordertest/MainActivity.kt
+++ app/src/main/java/com/example/recordertest/MainActivity.kt
@@ -2,22 +2,40 @@ package com.example.recordertest
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper.prepare
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
 
     private val recordButton : RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
-
+    // 메모리 관리로 null 로 둠
+    private var recorder : MediaRecorder ? = null
     private var state_record = State.BEFORE_RECORDING
-    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+        set(value){
+            field = value
+            recordButton.updateIconWithState(value)
+        }
+    private var player : MediaPlayer? = null
+    private val recordingFilePathway : String by lazy{
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +43,24 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission()
         initViews()
+        bindViews()
     }
 
 
-    /*  private val requestPermissionLauncher =
+      /*private val requestPermissionLauncher =
           registerForActivityResult(
               ActivityResultContracts.RequestPermission()
           ){
               isGranted: Boolean->
               if(isGranted) {
-                  onStart()
+
               }
               else{
                   finish()
               }
           }
           private fun requestAudioPermission() {
-              requestPermissionLauncher.launch(AUDIO_SERVICE)
+              requestPermissionLauncher.launch(MediaStore.Audio.Media.RECORD_SOUND_ACTION)
          }*/
 
       //deprecated 된 관계로 위로 대체 작업중
@@ -71,8 +90,70 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    // MediaRecorder apply deprecated 되었으므로 밑의 형식과 같이 바꾼다.
     private fun initViews() {
         recordButton.updateIconWithState(state_record)
+    }
+
+    private fun bindViews(){
+        recordButton.setOnClickListener{
+            when(state_record){
+                State.BEFORE_RECORDING ->{
+                    startRecording()
+                }
+                State.ON_RECORDING->{
+                    stopRecording()
+                }
+                State.AFTER_RECORDING ->{
+                    startPlaying()
+                }
+                State.ON_PLAYING ->{
+                    stopPlaying()
+                }
+            }
+        }
+    }
+
+    private fun startRecording(){
+        recorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        recorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        recorder?.setOutputFile(recordingFilePathway)
+        prepare()
+
+        recorder?.start()
+        state_record = State.ON_RECORDING
+    }
+    private fun startPlaying(){
+        try {
+            player?.setDataSource(recordingFilePathway)
+            player?.prepare()
+        }
+        catch (e : IOException)
+        {
+            println(e)
+
+        }
+        catch(e : IllegalArgumentException){
+            println(e)
+        }
+        player?.start();
+        state_record= State.ON_PLAYING
+    }
+    private fun stopPlaying(){
+        player?.stop();
+        player?.release()
+        player = null
+        state_record = State.AFTER_RECORDING
+    }
+
+    private fun stopRecording(){
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state_record = State.AFTER_RECORDING
     }
 
     companion object{
